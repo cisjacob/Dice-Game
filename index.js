3b633ef89cc614d1976a761e8e5e42c162b32ed5
Francis Jacob Taiño <70936835+cisjacob@users.noreply.github.com>
2021-07-17 16:31:38 +0800

    Added reset function.

--- index.js
+++ index.js
@@ -56,3 +56,34 @@ function flipCoin(){
     flipCount+=1;
     document.querySelector(".flipCount").innerText = flipCount;
 }
+
+function resetRoll(){
+    player1Count = 0;
+    player2Count = 0;
+    drawCount = 0;
+    rollCount = 0;
+
+    document.querySelector(".player1Count").innerText = player1Count;
+    document.querySelector(".player2Count").innerText = player2Count;
+    document.querySelector(".drawCount").innerText = drawCount;
+    document.querySelector(".rollCount").innerText = rollCount;
+
+    document.querySelector(".img1").setAttribute("src", "images/dice6.png");
+    document.querySelector(".img2").setAttribute("src", "images/dice6.png");
+    
+    document.querySelector("#h1Dice").innerText = "Dice Roller";
+}
+
+function resetFlip(){
+    headsCount = 0;
+    tailsCount = 0;
+    flipCount = 0;
+
+    document.querySelector(".headsCount").innerText = headsCount;
+    document.querySelector(".tailsCount").innerText = tailsCount;
+    document.querySelector(".flipCount").innerText = flipCount;
+
+    document.querySelector(".coin").setAttribute("src", "images/coin1.jpg");
+    
+    document.querySelector("#h1Coin").innerText = "Heads or tails?";
+}
